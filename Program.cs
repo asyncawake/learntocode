d8629310ab71a5f4ab372d1033fb253eb956e693
asyncawake <asyncawake@gmail.com>
2018-06-30 21:11:49 +0200

    Play with for loop.

--- Program.cs
+++ Program.cs
@@ -7,30 +7,39 @@ namespace learntocode
         static void Main(string[] args)
         {
             
-            // This is the "secret" number
-            int number = 4;
-
-            Console.WriteLine("I've chosen a secret number! can you gues it?!?!?");
-
-            int guess;
-            do
+            for (int i = 2; i < 10; i++)
             {
-                Console.WriteLine("What's your guess?");
-                string guessString = Console.ReadLine();
-                int.TryParse(guessString, out guess);
-
-                if (guess < number)
+                if (i == 4)
                 {
-                    Console.WriteLine("You are too low. Guess higher next time");
+                    Console.WriteLine("This one is four!");
                 }
+                Console.WriteLine(i);
+            }
 
-                if (guess > number)
-                {
-                    Console.WriteLine("You are too high. Guess lower next time");
-                }
+            // Random rand = new Random();
+            // int number = rand.Next(1,1_000); // chose a random number between 1 and a million. The underscores make large number more readable.
 
-            } while (guess != number);
-            Console.WriteLine("You got it!");
+            // Console.WriteLine("I've chosen a secret number between 1 and 1,000,000! Can you gues it?!?!?");
+
+            // int guess;
+            // do
+            // {
+            //     Console.WriteLine("What's your guess?");
+            //     string guessString = Console.ReadLine();
+            //     int.TryParse(guessString, out guess);
+
+            //     if (guess < number)
+            //     {
+            //         Console.WriteLine("You are too low. Guess higher next time");
+            //     }
+
+            //     if (guess > number)
+            //     {
+            //         Console.WriteLine("You are too high. Guess lower next time");
+            //     }
+
+            // } while (guess != number);
+            // Console.WriteLine("You got it!");
 
         }
     }

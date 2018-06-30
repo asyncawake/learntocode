3856e32eac01b99e4af079e3705f03393c7d48d7
asyncawake <asyncawake@gmail.com>
2018-06-30 20:52:50 +0200

    guess number first edition

--- Program.cs
+++ Program.cs
@@ -6,61 +6,32 @@ namespace learntocode
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("What's your name?");
-            string name = Console.ReadLine();
-            //string name = "DevChatter";
-            //Console.WriteLine($"I really like the name {name}!");
-            Console.WriteLine($"Hello {name}");
+            
+            // This is the "secret" number
+            int number = 4;
 
-            Console.WriteLine($"So, {name}, what's your favorite number?");
-            string rawText = Console.ReadLine();
-            //int unsafeInt = int.Parse(rawText);
-            int.TryParse(rawText, out int number);
+            Console.WriteLine("I've chosen a secret number! can you gues it?!?!?");
 
-            if (number < 5)
+            int guess;
+            do
             {
-                number = 5;
-                Console.WriteLine($"The minimum number is five, so I updated your choice to {number}");
-            }
+                Console.WriteLine("What's your guess?");
+                string guessString = Console.ReadLine();
+                int.TryParse(guessString, out guess);
 
-            // number = number +2;
-            // Console.WriteLine($"I like {number} better, so I changed to it for you.");
+                if (guess < number)
+                {
+                    Console.WriteLine("You are too low. Guess higher next time");
+                }
 
-            // Console.WriteLine($"You chose {number}.");
-            // int betterNumber = number + 1;
-            // Console.WriteLine($"I like the number {betterNumber} better. It's larger.");
+                if (guess > number)
+                {
+                    Console.WriteLine("You are too high. Guess lower next time");
+                }
 
-            // int i = 0;
-            // while (i < number)
-            // {
-            //     Console.WriteLine($"You are awesome, {name}!");
-            //     i++; // increments value of i
-            // }
+            } while (guess != number);
+            Console.WriteLine("You got it!");
 
-            
-            //(initialize; compare; increment)
-            for (int i = 0; i < number; i++)
-            {
-                Console.WriteLine($"You are awesome! {name}");
-            }
-
-            // object s= "7";
-            // object n = 7;
-            // Console.WriteLine(s == n);
-
-            
-
-            // if (number == 7)
-            // {
-            //     Console.WriteLine("That's great! Seven is a common favorite number!");
-            // }            
-
-            // if (number == 13)
-            // {
-            //     Console.WriteLine("That's strange! Many people are afraid of 13!");
-            // }
-            
-            
         }
     }
 }
